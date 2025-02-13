c4cceba154173a878d9126277dfe1b8a6772a393
Rohit Kumar <rokumar@conviva.com>
2025-02-13 15:43:11 +0530

    basic layout

--- MaangPatti/components/gamePage/GamePage.tsx
+++ MaangPatti/components/gamePage/GamePage.tsx
@@ -1,25 +1,53 @@
 import React from 'react';
 import styles from './GamePage.module.css';
+import { Image } from 'react-native';
 
 const GamePage: React.FC = () => {
   return (
     <div className={styles.container}>
       <div className={styles.topSection}>
-        {/* Top section content */}
+        <Image 
+          source={require('./1B.svg')}
+          style={{ width: 100, height: 140 }}
+          resizeMode="contain"
+        />
       </div>
 
       <div className={styles.middleSection}>
         <div className={styles.leftColumn}>
-          <div className={styles.leftBox}></div>
-          <div className={styles.leftBox}></div>
+          <div className={styles.leftBox}>
+          <Image 
+          source={require('./1B.svg')}
+          style={{ width: 100, height: 140 }}
+          resizeMode="contain"
+        />
+          </div>
+          <div className={styles.leftBox}>
+          <Image 
+          source={require('./1B.svg')}
+          style={{ width: 100, height: 140 }}
+          resizeMode="contain"
+        />
+          </div>
         </div>
         <div className={styles.rightColumn}>
-          <div className={styles.rightBox}></div>
+          <div className={styles.rightBox}>
+          <Image 
+          source={require('./1B.svg')}
+          style={{ width: 100, height: 140 }}
+          resizeMode="contain"
+        />
+          </div>
         </div>
       </div>
 
       <div className={styles.bottomSection}>
         {/* Bottom section content */}
+        <Image 
+          source={require('./1B.svg')}
+          style={{ width: 100, height: 140 }}
+          resizeMode="contain"
+        />
       </div>
     </div>
   );
